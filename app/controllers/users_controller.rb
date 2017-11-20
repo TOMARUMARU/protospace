51254738e9674814529e742d7b11642f16eb91bb
shoki <ac.milan.22x36@gmail.com>
2017-11-21 01:24:33 +0900

    flash_message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,15 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to root_path, notice: "User information changed"
     else
+      flash.now[:alert] = "Failed to update user information"
       render :edit
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :member, :profile, :works)
+    params.require(:user).permit(:name, :avatar, :email, :member, :profile, :works)
   end
 end
