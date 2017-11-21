f0c946b661c64b07df5cffa6541175d31de70386
shoki <ac.milan.22x36@gmail.com>
2017-11-21 18:23:33 +0900

    post_func

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :proto_images, dependent: :destroy
+  accepts_nested_attributes_for :proto_images, allow_destroy: true
+  validates :title, :catch_copy, :concept, presence: true
 end
