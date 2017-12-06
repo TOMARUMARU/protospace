c865a43cf4a678a6eff95c197aace8c2253ab5a0
shoki <ac.milan.22x36@gmail.com>
2017-12-06 20:44:07 +0900

    create_like-model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121084224) do
+ActiveRecord::Schema.define(version: 20171206113802) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "likes", ["prototype_id"], name: "fk_rails_8847d87628", using: :btree
+  add_index "likes", ["user_id"], name: "fk_rails_1e09b5dabf", using: :btree
 
   create_table "proto_images", force: :cascade do |t|
     t.text     "image",        limit: 65535,             null: false
@@ -58,6 +68,8 @@ ActiveRecord::Schema.define(version: 20171121084224) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "likes", "prototypes"
+  add_foreign_key "likes", "users"
   add_foreign_key "proto_images", "prototypes"
   add_foreign_key "prototypes", "users"
 end
