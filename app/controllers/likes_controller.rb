13c2309886d72016d2889d841bc522d523b1a668
shoki <ac.milan.22x36@gmail.com>
2017-12-12 14:48:56 +0900

    fix_likes-controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,18 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @like = current_user.likes.create(like_params)
     @prototype = Prototype.find(params[:prototype_id])
+
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
+    like = current_user.likes.find_by(like_params)
     like.destroy
     @prototype = Prototype.find(params[:prototype_id])
+  end
+
+  private
+  def like_params
+    params.permit(:prototype_id)
   end
 end
