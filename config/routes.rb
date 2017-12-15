6fba82a75a52e62a8913a5b46e473af731d7f8f6
shoki <ac.milan.22x36@gmail.com>
2017-12-15 14:33:55 +0900

    delete_popular-routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
     resource :likes, only: %i(create destroy)
   end
   resources :users, only: %i(show edit update)
-  resources :populars, only: %i(index)
   resources :newests, only: %i(index)
 end
