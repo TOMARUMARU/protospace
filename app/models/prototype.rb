882793ece58c22f8d5c730cb377abe212d5786dd
shoki <ac.milan.22x36@gmail.com>
2017-12-17 19:20:04 +0900

    add_association

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :proto_images, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :proto_images, allow_destroy: true
   validates :title, :catch_copy, :concept, presence: true
 
