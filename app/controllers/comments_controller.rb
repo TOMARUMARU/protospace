31761c816bcff4c189bb2f692510a8ab583bc61b
shoki <ac.milan.22x36@gmail.com>
2017-12-22 18:24:50 +0900

    write_comments-controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
-  def new
+  def create
+    @prototype = Prototype.find(params[:prototype_id])
+    current_user.comments.create(comment_params)
   end
 
-  def create
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(prototype_id: params[:prototype_id])
   end
 end
